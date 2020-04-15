0c19ade5e7446cced68c83ca89af8695bb785fc3
filipwojcik <filip.igor.wojcik@gmail.com>
2020-04-15 18:00:36 +0200

    Started implementation of complex crossval procedure for experiment data

--- run_experiment.py
+++ run_experiment.py
@@ -1,5 +1,5 @@
-from src.data.train_data_utils import prepare_experiment_data
+from src.data.train_data_utils import prepare_experiment
 
 if __name__ == "__main__":
-    experiment_data = prepare_experiment_data(False)
+    experiment_data = prepare_experiment(False)
     print('done')
--- src/data/train_data_utils.py
+++ src/data/train_data_utils.py
@@ -1,10 +1,13 @@
+from typing import List
+
 import pandas as pd
 import numpy as np
 
+from enum import Enum
 from collections import namedtuple
 from scipy.sparse import lil_matrix, csr_matrix
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 
 from src.data.make_dataset import load_dataset
 from src.features.build_features import get_dummy_values, fill_missing_values, FEATURE_COLUMNS
@@ -19,7 +22,7 @@ RATING = 'rating'
 
 ITEM_ID_COL = 'item_id'
 USER_ID_COL = 'user_id'
-#COLUMNS_TO_DROP = [RATING, 'year', ITEM_ID_COL, 'split', 'category', 'timestamp', USER_ID_COL]
+# COLUMNS_TO_DROP = [RATING, 'year', ITEM_ID_COL, 'split', 'category', 'timestamp', USER_ID_COL]
 COLUMNS_TO_DROP = [RATING, ITEM_ID_COL, 'split', 'timestamp', USER_ID_COL]
 
 ExperimentData = namedtuple(
@@ -44,6 +47,12 @@ ExperimentData = namedtuple(
         "feature_names"
     ]
 )
+
+
+class DataType(Enum):
+    TRAIN = 'train'
+    TEST = 'test'
+    VALID = 'valid'
 
 
 def make_id_column_consecutive_integer_from_0(data, col, new_colname=None):
@@ -122,9 +131,28 @@ def hide_test_data_ratings_for_prediction(Xr_test, X_test_raw, perc_test=0.3, mi
         Xr_test_pred_hidden[uid, item_id] = 0.0
         X_test_raw_pred_hidden.loc[
             (X_test_raw_pred_hidden[uid_consecutive_colname] == uid) & (
-                        X_test_raw_pred_hidden[iid_colname] == item_id)] = 0.0
+                    X_test_raw_pred_hidden[iid_colname] == item_id)] = 0.0
 
     return uids_consecutive, uids_original, item_ids, y, Xr_test_pred_hidden, X_test_raw_pred_hidden
+
+
+def crossval_generator(data: pd.DataFrame,
+                       test_size: float = 0.4,
+                       test_seed: int = 123,
+                       valid_seed: int = 456,
+                       test_ratigs_perc_to_hide: float = 0.3,
+                       test_ratings_to_hide_seed: int = 999,
+                       nsplits: int = 10) -> List[ExperimentData]:
+    data = make_id_column_consecutive_integer_from_0(data, 'user_id')
+    data = make_id_column_consecutive_integer_from_0(data, 'item_id')
+    data.sort_values(by=['user_id', 'item_id'], ascending=True)
+    nuser, nitem = data.user_id.max() + 1, data.item_id.max() + 1
+
+    kf = KFold(n_splits=nsplits, random_state=valid_seed)
+    for train_ids, test_val_ids in kf.split(data.user_id.unique()):
+        yield prepare_experiment_data_from_sample(data, nitem, nuser, test_ratigs_perc_to_hide,
+                                                  test_ratings_to_hide_seed,
+                                                  test_seed, test_size, test_val_ids, train_ids)
 
 
 def train_validation_test_split(
@@ -142,53 +170,70 @@ def train_validation_test_split(
 
     train_ids, test_val_ids = train_test_split(data.user_id.unique(), test_size=valid_size, random_state=valid_seed)
 
+    return prepare_experiment_data_from_sample(data, nitem, nuser, test_ratigs_perc_to_hide, test_ratings_to_hide_seed,
+                                               test_seed, test_size, test_val_ids, train_ids)
+
+
+def prepare_experiment_data_from_sample(
+        data,
+        nitem,
+        nuser,
+        test_ratigs_perc_to_hide,
+        test_ratings_to_hide_seed,
+        test_seed,
+        test_size,
+        test_val_ids,
+        train_ids):
     X_train_raw = data.loc[data.user_id.isin(train_ids)]
     test_ids, validation_ids = train_test_split(test_val_ids, test_size=test_size, random_state=test_seed)
+
     X_test_raw, X_val_raw = data.loc[data.user_id.isin(test_ids)], \
                             data.loc[data.user_id.isin(validation_ids)]
 
-    X_train_raw.reset_index(inplace=True, drop=True)
-    X_train_raw = make_id_column_consecutive_integer_from_0(X_train_raw, USER_ID_COL, UID_TRAIN_COL)
+    raw_data_sets = {
+        DataType.TRAIN: (X_train_raw, UID_TRAIN_COL),
+        DataType.TEST: (X_test_raw, UID_TEST_COL),
+        DataType.VALID: (X_val_raw, UID_VALC_COL)
+    }
+    processed_data = {}
+    ratings_data = {}
+    features_data = {}
 
-    X_test_raw.reset_index(inplace=True, drop=True)
-    X_test_raw = make_id_column_consecutive_integer_from_0(X_test_raw, USER_ID_COL, UID_TEST_COL)
+    feature_names = []
 
-    X_val_raw.reset_index(inplace=True, drop=True)
-    X_val_raw = make_id_column_consecutive_integer_from_0(X_val_raw, USER_ID_COL, UID_VALC_COL)
+    for dtype, (dset, new_uid_col) in raw_data_sets.items():
+        processed_raw = make_id_column_consecutive_integer_from_0(dset.reset_index(drop=True), USER_ID_COL, new_uid_col)
+        ratings_ds = prepare_sparse_ratings_matrix(processed_raw, nuser, nitem, new_uid_col)
+        features_ds = processed_raw.drop(COLUMNS_TO_DROP + [new_uid_col], axis=1)
+        if feature_names == []:
+            feature_names = list(features_ds.columns)
+        features_matrix = features_ds.to_numpy()
 
-    assert X_train_raw.shape[0] + X_test_raw.shape[0] + X_val_raw.shape[0] == data.shape[0]
+        processed_data[dtype] = processed_raw
+        ratings_data[dtype] = ratings_ds
+        features_data[dtype] = features_matrix
 
-    feature_names = list(X_train_raw.drop(COLUMNS_TO_DROP + [UID_TRAIN_COL], axis=1).columns)
-
-    Xr_train = prepare_sparse_ratings_matrix(X_train_raw, nuser, nitem, UID_TRAIN_COL)
-    Xf_train = X_train_raw.drop(COLUMNS_TO_DROP + [UID_TRAIN_COL], axis=1).to_numpy()
-
-    Xr_test = prepare_sparse_ratings_matrix(X_test_raw, nuser, nitem, UID_TEST_COL)
-    Xf_test = X_test_raw.drop(COLUMNS_TO_DROP + [UID_TEST_COL], axis=1).to_numpy()
-
-    Xr_val = prepare_sparse_ratings_matrix(X_val_raw, nuser, nitem, UID_VALC_COL)
-    Xf_val = X_val_raw.drop(COLUMNS_TO_DROP + [UID_VALC_COL], axis=1).to_numpy()
+    assert sum([ds.shape[0] for ds in processed_data.values()]) == data.shape[0]
 
     uids, uids_original, item_ids, y, Xr_test_pred_hidden, X_test_raw_pred_hidden = hide_test_data_ratings_for_prediction(
-        Xr_test,
-        X_test_raw[[USER_ID_COL, UID_TEST_COL, ITEM_ID_COL, RATING]],
+        ratings_data[DataType.TEST],
+        processed_data[DataType.TEST][[USER_ID_COL, UID_TEST_COL, ITEM_ID_COL, RATING]],
         perc_test=test_ratigs_perc_to_hide,
         random_state=test_ratings_to_hide_seed,
         uid_consecutive_colname=UID_TEST_COL
     )
-
     return ExperimentData(
-        Xr_train,
+        ratings_data[DataType.TRAIN],
         Xr_test_pred_hidden,
-        Xr_val,
+        ratings_data[DataType.VALID],
 
-        Xf_train,
-        Xf_test,
-        Xf_val,
+        features_data[DataType.TRAIN],
+        features_data[DataType.TEST],
+        features_data[DataType.VALID],
 
-        X_train_raw[[USER_ID_COL, UID_TRAIN_COL, ITEM_ID_COL, RATING]],
+        processed_data[DataType.TRAIN][[USER_ID_COL, UID_TRAIN_COL, ITEM_ID_COL, RATING]],
         X_test_raw_pred_hidden,
-        X_val_raw[[USER_ID_COL, UID_VALC_COL, ITEM_ID_COL, RATING]],
+        processed_data[DataType.VALID][[USER_ID_COL, UID_VALC_COL, ITEM_ID_COL, RATING]],
 
         uids,
         uids_original,
@@ -198,9 +243,17 @@ def train_validation_test_split(
     )
 
 
-def prepare_experiment_data(download_new=False, test_seed:int=123, valid_seed:int=456) -> ExperimentData:
+def prepare_experiment(download_new=False, test_seed: int = 123, valid_seed: int = 456) -> ExperimentData:
     raw_data = load_dataset(download_new)
     raw_data = get_dummy_values(fill_missing_values(raw_data))
     data_min_n_ratings = get_users_with_min_n_ratings(raw_data, 3)
     experiment_data = train_validation_test_split(data_min_n_ratings, test_seed=test_seed, valid_seed=valid_seed)
     return experiment_data
+
+
+def prepare_crossval_experiment(download_new=False, test_size: float = 0.4, test_seed: int = 123, valid_seed: int = 456, nsplits: int = 10):
+    raw_data = load_dataset(download_new)
+    raw_data = get_dummy_values(fill_missing_values(raw_data))
+    data_min_n_ratings = get_users_with_min_n_ratings(raw_data, 3)
+    for exd in crossval_generator(raw_data, test_size, test_seed, valid_seed, nsplits=nsplits):
+        yield exd
